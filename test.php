f5b681701f9ab38c29cf291da27dd53634de2550
chame1215 <chameleon12151223@gmail.com>
2022-04-03 16:16:18 +0900

    init

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+print "<link rel=\"stylesheet\" href=\"./css/design.css\">";
+
+print <<<HTML
+<div class="main">
+	<div class="container top">
+		<h1>test1</h1>
+	</div>
+	<div class="container center">
+		<h1>test2</h1>
+	</div>
+	<div class="container bottom">
+		<h1>test3</h1>
+	</div>
+</div>	
+HTML;
+
+?>
